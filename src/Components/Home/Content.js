dec7d21c4c2fa6b4471988d9680222235aeaf47b
thedevilx <aadithya794@gmail.com>
2020-08-27 02:53:05 +0530

    Created Introduction Section for Home Page

---
+++ src/Components/Home/Content.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './styles';
+import { View, Text, Image, Dimensions } from 'react-native';
+import contentJson from '../../content/home.json';
+
+const { width, height } = Dimensions.get('window');
+
+function Content() {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+export default Content;
---
+++ src/Components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Content from './Content';
+import styles from './styles';
+import {View} from 'react-native';
+
+
+function Home() {
+    return (
+        <View>
+            <Content />
+        </View>
+    );
+}
+
+export default Home;
